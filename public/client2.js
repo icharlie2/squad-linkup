84707abc33c769e389e26998172e136bc8a3567f
System <none>
2017-08-12 10:23:51 +0000

    🙋🐆 Updated with Glitch v0.0.2

--- public/client2.js
+++ public/client2.js
@@ -2,39 +2,39 @@ $(document).ready(() => {
   
   //jQuery to navigate through the menu.
   $(".how-to-button").on("click", function(e) {
-    $(".btn").css("display", "none");
+    $(".btns").css("display", "none");
     $(".how-to-info").css("display", "inline-block");
     $(".back-button-1").css("display", "inline-block");
   });
   
   $(".back-button-1").on("click", function(e) {
-    $(".btn").css("display", "none");
+    $(".btns").css("display", "none");
     $(".how-to-info").css("display", "none");
     $(".how-to-button").css("display", "inline-block");
     $(".play-button").css("display", "inline-block");
   });
   
   $(".play-button").on("click", function(e) {
-    $(".btn").css("display", "none");
+    $(".btns").css("display", "none");
     $(".join-room").css("display", "inline-block");
     $(".make-room").css("display", "inline-block");
     $(".back-button-2").css("display", "inline-block");
   });
   
   $(".back-button-2").on("click", function(e) {
-    $(".btn").css("display", "none");
+    $(".btns").css("display", "none");
     $(".how-to-button").css("display", "inline-block");
     $(".play-button").css("display", "inline-block");
   });
   
   $(".join-room").on("click", function(e) {
-    $(".btn").css("display", "none");
+    $(".btns").css("display", "none");
     $(".room").css("display", "inline-block");
     $(".back-button-3").css("display", "inline-block");
   });
   
   $(".back-button-3").on("click", function(e) {
-    $(".btn").css("display", "none");
+    $(".btns").css("display", "none");
     $(".room").css("display", "none");
     $(".join-room").css("display", "inline-block");
     $(".make-room").css("display", "inline-block");
